8b9f2f015f1b04ab7e648ab285732a06c4908d0e
Nils Hörrmann <post@nilshoerrmann.de>
2010-08-07 18:32:16 +0200

    Adjust upgrade information

--- content/content.index.php
+++ content/content.index.php
@@ -18,10 +18,10 @@
 			$introduction = new XMLElement(
 				'fieldset', 
 				'<legend>Introduction</legend>
-				<p>Subsection Manager is a replacement of the Mediathek field introducing a new interface and an improved feature set. Subsection Manager requires Symphony 2.1 and should not be used side-by-side with Mediathek. This page will help you upgrading your fields from Mediathek to Subsection Manager.</p>
-				<h3>Before you proceed</h3>
+				<p>Subsection Manager is a replacement of the Mediathek field, introducing a new interface and an improved feature set. Subsection Manager requires Symphony 2.1 and should not be used side-by-side with Mediathek. This page will help you upgrading your fields from Mediathek to Subsection Manager.</p>
+				<h2>Before you proceed</h2>
 				<ul>
-					<li>Please make sure that you have an up-to-date backup of your site containing all files and folders and a copy of your database. <strong>If you don&#8217;t have a backup, create one now!</strong></li>
+					<li>Please make sure that you have an up-to-date backup of your site, containing all files and folders, and a copy of your database. <strong>If you don&#8217;t have a backup, create one now!</strong> </li>
 					<li>Upgrading your Mediathek fields to Subsection Manager will alter your database. <strong>This process cannot be undone!</strong></li>
 				</ul>', 
 				array(
@@ -36,7 +36,7 @@
 				'<legend>Upgrading your Mediathek</legend>
 				<p>The Subsection Manager Upgrade will automatically perform the following actions:</p>
 				<ol>
-					<li>Replace all Mediathek fields with the Subsection Manager copying all attached information to the new fields.</li>
+					<li>Replace all Mediathek fields with the Subsection Manager, copying all attached information to the new fields.</li>
 					<li>Uninstall the Mediathek extension removing all references in the database.</li>
 				</ol>
 				<p>The Mediathek folder will stay intact in your extension folder. You will have to delete it manually.</p>', 
