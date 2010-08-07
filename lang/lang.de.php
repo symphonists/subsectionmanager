5e15b0fec3742f109aac2237b68a15928795f079
Nils Hörrmann <post@nilshoerrmann.de>
2010-08-07 17:39:18 +0200

    Update German translation

--- lang/lang.de.php
+++ lang/lang.de.php
@@ -5,9 +5,9 @@
 		'author' => array(
 			'name' => 'Nils Hörrmann',
 			'email' => 'post@nilshoerrmann.de',
-			'website' => 'http://www.nilshoerrmann.de'
+			'website' => 'http://nilshoerrmann.de'
 		),
-		'release-date' => '2010-07-27',
+		'release-date' => '2010-08-07',
 	);
 
 
@@ -16,11 +16,35 @@
 	 */
 	$dictionary = array(
 
+		'%s Show thumbnail images' => 
+		'%s Zeige Vorschaubilder',
+
 		'Are you sure you want to delete this item? It will be remove from all entries. This step cannot be undone.' => 
 		'Sind Sie sicher, dass Sie diesen Eintrag löschen wollen? Er wird von allen verknüpften Einträgen entfernt. Dieser Vorgang kann nicht rückgängig gemacht werden.',
 
+		'Behaviour' => 
+		'Verhalten',
+
+		'Caption' => 
+		'Bezeichnung',
+
 		'Caption has to be well-formed. Please check opening and closing tags.' => 
 		'Der Eintragstitel muss wohlgeformt sein. Bitte überprüfen Sie öffnende und schließende Tags.',
+
+		'Comma separated, alt+click for negation' => 
+		'kommagetrennt, für Verneinung alt+Klick',
+
+		'Data Source XML' => 
+		'Datenquellen-XML',
+
+		'Disable Mediathek' => 
+		'Mediathek deaktivieren',
+
+		'Disable Subsection Manager' => 
+		'Subsection Manager deaktivieren',
+
+		'Display' => 
+		'Darstellung',
 
 		'Filter items by tags or categories' => 
 		'Einträge nach Tags oder Kategorien filtern',
@@ -40,11 +64,26 @@
 		'Remove Item' => 
 		'Eintrag entfernen',
 
+		'Subsection' => 
+		'Unterbereich',
+
+		'Subsection Manager' => 
+		'Subsection Manager',
+
+		'Subsection Manager Upgrade' => 
+		'Subsection-Manager-Aktualisierung',
+
 		'There are no selected items' => 
 		'Keine ausgewählten Einträge vorhanden',
 
+		'Upgrade' => 
+		'Mediathek aktualisieren',
+
 		'Use comma separated entry ids for filtering.' => 
 		'Verwenden Sie kommagetrennte Eintrags-IDs, um die Ausgabe zu filtern.',
+
+		'You are using Mediathek and Subsection Manager simultaneously.' => 
+		'Sie verwenden die Mediathek un den Subsection Manager gleichzeitig.',
 
 		'item' => 
 		'Eintrag',
@@ -52,28 +91,17 @@
 		'items' => 
 		'Einträge',
 
-		// Obsolete
-
-		'%s Preview images and files' => 
-		'%s Vorschau für Bilder und Dateien einblenden',
-
 		'Browse' => 
 		'Durchsuchen',
 
 		'Click here to create a new item.' => 
-		'Klicken Sie hier um einen neuen Eintrag zu erstellen.',
+		'Klicken Sie hier, um einen neuen Eintrag zu erstellen.',
 
-		'Comma separated, click alt for negation' => 
-		'kommagetrennt, für Verneinung alt klicken',
-
-		'Custom item caption' => 
-		'Benutzerdefinierte Eintragstitel',
-
-		'Related section' => 
-		'Verknüpfter Bereich',
+		'Don\'t forget to include the Subsection Manager field in your Data Source' => 
+		'Vergessen Sie nicht, das Subsection-Manager-Feld in Ihre Datenquelle einzubinden',
 
 		'There are currently no items available. Perhaps you want create one first?' => 
-		'Zur Zeit sind keine Einträge verfügbar. Möchte Sie zunächst einen erstellen?',
+		'Zur Zeit sind keine Einträge verfügbar. Möchten Sie zunächst einen erstellen?',
 
 		'This is a required field.' => 
 		'Dies ist ein Pflichtfeld',
@@ -81,90 +109,55 @@
 		'Untitled' => 
 		'Ohne Titel',
 
-		'Use <code>{$param}</code> syntax, inline HTML elements allowed' => 
-		'Verwenden Sie <code>{$param}</code>, Inline-HTML-Element erlaubt',
-
-		'Will be used for data source output' => 
-		'Wird für die Datenquellenausgabe verwendet',
-
-		// Missing
-
-		'Subsection Manager Upgrade' => 
-		'Subsection-Manager-Aktualisierung',
-
-		'Subsection Manager' => 
-		'Subsection Manager',
-
-		'Subsection' => 
-		'Unterbereich',
-
-		'Comma separated, alt+click for negation' => 
-		'kommagetrennt, für Verneinung alt+Klick',
-
-		'Behaviour' => 
-		'Verhalten',
-
 		'Allow creation of new items' => 
-		false,
+		'Erstellen von Einträgen zulassen',
 
 		'This will add a <code>Create New</code> button to the interface' => 
-		false,
+		'Dies fügt einen <code>Neu-erstellen</code>-Knopf hinzu',
 
 		'Allow deselection of items' => 
-		false,
+		'Abwahl von Einträgen zulassen',
 
 		'This will add a <code>Remove</code> button to the interface' => 
-		false,
+		'Dies fügt einen <code>Löschen</code>-Knopf hinzu',
 
 		'Allow selection of items from a list of existing items' => 
-		false,
+		'Auswahl aus einer Liste bereits bestehender Einträge zulassen',
 
 		'This will add a search field to the interface' => 
-		false,
+		'Dies fügt ein Suchfeld hinzu',
 
 		'Allow dropping of items' => 
-		false,
+		'Einfügen von Einträge erlauben',
 
 		'This will enable item dropping on textareas' => 
-		false,
+		'Dies ermöglicht das Ziehen von Einträgen in Textfelder',
 
 		'Allow selection of multiple items' => 
-		false,
+		'Auswahl mehrerer Einträge zulassen',
 
 		'This will switch between single and multiple item lists' => 
-		false,
+		'Dies wechselt zwischen Einzel- und Mehrfacheinträgen',
 
 		'Allow sorting of items' => 
-		false,
+		'Sortieren von Einträgen erlauben.',
 
 		'This will enable item dragging and reordering' => 
-		false,
-
-		'Display' => 
-		'Darstellung',
-
-		'Caption' => 
-		'Bezeichnung',
-
-		'%s Show thumbnail images' => 
-		'%s Zeige Vorschaubilder',
-
-		'Data Source XML' => 
-		'Datenquellen-XML',
+		'Dies ermöglicht verschieben und neuordnen von Einträgen',
 
 		'Don\'t forget to include the Subsection Manager field in your Data Source' => 
-		'Vergessen Sie nicht, das Subsection-Manager-Feld in Ihre Datenquelle einzubinden',
+		'Vergessen Sie nicht das Subsection-Manager-Feld Ihrer Datenquelle hinzuzufügen',
 
-		'You are using Mediathek and Subsection Manager simultaneously.' => 
-		'Sie verwenden die Mediathek un den Subsection Manager gleichzeitig.',
+		'Submodule %s is missing.' => 
+		'Das Untermodul %s fehlt.',
 
-		'Upgrade' => 
-		'Mediathek aktualisieren',
+		'Please add the missing submodules to %s. ' => 
+		'Bitte fügen Sie die fehlenden Untermodul %s hinzu. ',
 
-		'Disable Mediathek' => 
-		'Mediathek deaktivieren',
+		'Please add the missing submodule to %s. ' => 
+		'Bitte fügen Sie das fehlende Untermodul %s hinzu. ',
 
-		'Disable Subsection Manager' => 
-		'Subsection Manager deaktivieren',
+		'For further assistence have a look at the documentation available on %s.' => 
+		'Weitere Hilfestellungen finden Sie in der Dokumentation auf %s.',
 
 	);
