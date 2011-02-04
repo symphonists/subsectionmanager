c3eed44f154d3062c213b79d2be1831c7d1fbd67
Vlad <vlad_micutul@yahoo.com>
2011-02-04 22:28:21 +0800

    Added romanian translation

---
+++ lang/lang.ro.php
@@ -0,0 +1,199 @@
+<?php
+
+	$about = array(
+		'name' => 'Română',
+		'author' => array(
+			'name' => 'Vlad Ghita',
+			'email' => 'vlad_micutul@yahoo.com',
+			'website' => ''
+		),
+		'release-date' => '2010-11-12',
+	);
+
+
+	/**
+	 * Subsection Manager
+	 */
+	$dictionary = array(
+
+		'Subsection Manager' => 
+		'Administrator Subsecţiune',
+
+		'There are no selected items' => 
+		'Nu există elemente selectate',
+
+		'Are you sure you want to delete this item? It will be remove from all entries. This step cannot be undone.' => 
+		'Sigur doreşti ştergerea acestui element? Va fi şters din toate înregistrările. Acest pas este ireversibil',
+
+		'I have a working backup of my site including files and database and like to upgrade all my Mediathek fields to Subsection Manager fields.' => 
+		false,
+
+		'Subsection Manager Upgrade' => 
+		'Upgrade pentru Subsection Manager',
+
+		'Introduction' => 
+		'Introducere',
+
+		'Subsection Manager is a replacement of the Mediathek field, introducing a new interface and an improved feature set. Subsection Manager requires Symphony 2.1 and should not be used side-by-side with Mediathek. This page will help you upgrading your fields from Mediathek to Subsection Manager.' => 
+		false,
+
+		'Before you proceed' => 
+		'Înainte de a continua',
+
+		'Please make sure that you have an up-to-date backup of your site, containing all files and folders, and a copy of your database.' => 
+		'Asiguraţi-vă că aveţi o copie de rezervă la zi a site-ului, conţinând toate fişierele si directoarele şi o copie a bazei de date.',
+
+		'If you don&#8217;t have a backup, create one now!' => 
+		'Dacă nu a veţi o copie de rezervă, faceţi una acum!',
+
+		'Upgrading your Mediathek fields to Subsection Manager will alter your database.' => 
+		false,
+
+		'This process cannot be undone!' => 
+		'Acest proces este ireversibil',
+
+		'Upgrading your Mediathek' => 
+		false,
+
+		'The Subsection Manager Upgrade will automatically perform the following actions:' => 
+		'Actualizarea Administratorului de Subsecţiune va efectua automat următoarele acţiuni:',
+
+		'Replace all Mediathek fields with the Subsection Manager, copying all attached information to the new fields.' => 
+		false,
+
+		'Uninstall the Mediathek extension removing all references in the database.' => 
+		false,
+
+		'The Mediathek folder will stay intact in your extension folder. You will have to delete it manually.' => 
+		false,
+
+		'Upgrade all Mediathek fields' => 
+		false,
+
+		'Upgrade successful' => 
+		'Actualizare reuşită',
+
+		'Your Mediathek fields have successfully been upgrade to Subsection Manager.' => 
+		false,
+
+		'Please delete the old Mediathek folder from your <code>extensions</code> folder.' => 
+		false,
+
+		'Go back to the extension overview.' => 
+		'Reîntoarceţi-vă la descriere extensiei.',
+
+		'Subsection' => 
+		'Subsecţiune',
+
+		'None Selected' => 
+		'Niciun element selectat',
+
+		'Filter items by tags or categories' => 
+		'Filtrează elementele după etichete sau categorii',
+
+		'Comma separated, alt+click for negation' => 
+		'Separare prin virgulă, alt+clic pentru negare',
+
+		'Behaviour' => 
+		'Comportament',
+
+		'Allow creation of new items' => 
+		'Permite crearea de noi elemente',
+
+		'This will add a <code>Create New</code> button to the interface' => 
+		'Aceasta va adăuga un buton <code>Crează Element</code> la interfaţă',
+
+		'Allow deselection of items' => 
+		'Permite deselectarea de elemente',
+
+		'This will add a <code>Remove</code> button to the interface' => 
+		'Aceasta va adăuga un buton <code>Şterge</code> la interfaţă',
+
+		'Allow selection of items from a list of existing items' => 
+		'Permite selectarea elementelor dintr-o listă de elemente existente',
+
+		'This will add a search field to the interface' => 
+		'Aceasta va adăuga un câmp de căutare la interfaţă',
+
+		'Allow dropping of items' => 
+		'Permite ataşarea de elemente',
+
+		'This will enable item dropping on textareas' => 
+		'Aceasta va permite ataşarea de elemente pe câmpurile de text',
+
+		'Allow selection of multiple items' => 
+		'Permite selectarea mai multor elemente',
+
+		'This will switch between single and multiple item lists' => 
+		'Aceasta va schimba între liste de elemente simple şi multiple',
+
+		'Allow sorting of items' => 
+		'Permite sortarea elementelor',
+
+		'This will enable item dragging and reordering' => 
+		'Aceasta permite reordonarea elementelor',
+
+		'Display' => 
+		'Afişează',
+
+		'Caption' => 
+		'Descriere',
+
+		'%s Show thumbnail images' => 
+		'%s Arată imaginile în miniatură',
+
+		'Data Source XML' => 
+		'XML-ul Sursei de Date',
+
+		'Included elements' => 
+		'Elementele incluse',
+
+		'Don&#8217;t forget to include the Subsection Manager field in your Data Source' => 
+		'Nu uita să incluzi câmpul Administrator de Subsecţiune în Sursa ta de Date.',
+
+		'Caption has to be well-formed. Please check opening and closing tags.' => 
+		'Descrierea trebuie să fie bine formatată. Verificaţi tag-urile de început şi sfârşit.',
+
+		'Submodule %s is missing.' => 
+		'Submodulul %s lipseşte',
+
+		'Please add the missing submodules to %s. ' => 
+		'La %s trebuie adăugate submodulele lipsă.',
+
+		'Please add the missing submodule to %s. ' => 
+		'La %s trebuie adăugat submodulul lipsă.',
+
+		'For further assistence have a look at the documentation available on %s.' => 
+		'Pentru mai multe detalii accesaţi documentaţia disponibilă la %s.',
+
+		'New item' => 
+		'Element Nou',
+
+		'Please fill out the form below.' => 
+		'Completaţi formularul de mai jos.',
+
+		'Remove Item' => 
+		'Şterge Element',
+
+		'items' => 
+		'elemente',
+
+		'item' => 
+		'element',
+
+		'Use comma separated entry ids for filtering.' => 
+		'Pentru filtrare, folosiţi id-urile de la intrări, separate prin virgulă.',
+
+		'You are using Mediathek and Subsection Manager simultaneously.' => 
+		'',
+
+		'Upgrade' => 
+		'Actualizează',
+
+		'Uninstall Mediathek' => 
+		'',
+
+		'Uninstall Subsection Manager' => 
+		'Dezinstalează Administratorul de Subsecţiune',
+
+	);
