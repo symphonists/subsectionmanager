c28c379d541c4c341b848e37fcd516cff45976e2
Nils Hörrmann <post@nilshoerrmann.de>
2012-05-10 13:20:26 +0200

    Update German translation

--- lang/lang.de.php
+++ lang/lang.de.php
@@ -7,7 +7,7 @@
 			'email' => 'post@nilshoerrmann.de',
 			'website' => 'http://nilshoerrmann.de'
 		),
-		'release-date' => '2011-02-09'
+		'release-date' => '2012-05-10'
 	);
 
 	/**
@@ -15,199 +15,121 @@
 	 */
 	$dictionary = array(
 
-		'%s Show thumbnail images' =>
-		'%s Zeige Vorschaubilder',
-
-		'%s has to be well-formed. Please check opening and closing tags.' =>
+		'%s has to be well-formed. Please check opening and closing tags.' => 
 		'Dieses Feld muss wohlgeformt sein. Bitte überprüfen Sie öffnende und schließende Tags.',
 
-		'1 match' =>
+		'1 match' => 
 		'1 Treffer',
 
-		'Allow creation of new items' =>
+		'Allow creation of new items' => 
 		'Erstellen von Einträgen zulassen',
 
-		'Allow deselection of items' =>
-		'Abwahl von Einträgen zulassen',
-
-		'Allow dropping of items' =>
-		'Einfügen von Einträge erlauben',
-
-		'Allow selection of items from a list of existing items' =>
-		'Auswahl aus einer Liste bereits bestehender Einträge zulassen',
-
-		'Allow selection of multiple items' =>
-		'Auswahl mehrerer Einträge zulassen',
-
-		'Allow sorting of items' =>
-		'Sortieren von Einträgen erlauben.',
-
-		'Are you sure you want to delete {$item}? It will be removed from all entries. This step cannot be undone.' =>
+		'Are you sure you want to delete {$item}? It will be removed from all entries. This step cannot be undone.' => 
 		'Sind Sie sicher, dass Sie {$item} löschen wollen? Er wird von allen verknüpften Einträgen entfernt. Dieser Vorgang kann nicht rückgängig gemacht werden.',
 
-		'Before you proceed' =>
-		'Bevor Sie fortfahren',
-
-		'Behaviour' =>
-		'Verhalten',
-
-		'Browse' =>
-		'Durchsuchen',
-
-		'Caption' =>
+		'Caption' => 
 		'Bezeichnung',
 
-		'Comma separated, alt+click for negation' =>
+		'Comma separated, alt+click for negation' => 
 		'kommagetrennt, für Verneinung alt+Klick',
 
-		'Data Source XML' =>
-		'Datenquellen-XML',
-
-		'Display' =>
-		'Darstellung',
-
-		'Don&#8217;t forget to include the Subsection Manager field in your Data Source' =>
-		'Vergessen Sie nicht das Subsection-Manager-Feld Ihrer Datenquelle hinzuzufügen',
-
-		'Drop text' =>
+		'Drop text' => 
 		'Droptext',
 
-		'Filter items by tags or categories' =>
+		'Filter items by tags or categories' => 
 		'Einträge nach Tags oder Kategorien filtern',
 
-		'Go back to the extension overview.' =>
-		'Zurück zur Übersicht der Erweiterungen.',
-
-		'I have a working backup of my site including files and database and like to upgrade all my Mediathek fields to Subsection Manager fields.' =>
-		'Ich habe eine funktionsfähige Sicherungskopie meiner Installation inklusive aller Dateien und der Datenbank und möchte alle meine Mediathek-Felder auf den Subsection Manager aktualisieren.',
-
-		'If you don&#8217;t have a backup, create one now!' =>
-		'Wenn Sie kein Backup habe, erstellen Sie jetzt eines!',
-
-		'Included elements' =>
-		'Eingebundene Elemente',
-
-		'Introduction' =>
-		'Einführung',
-
-		'New item' =>
+		'New item' => 
 		'Neuer Eintrag',
 
-		'No items found.' =>
-		'Keine Einträge gefunden',
-
-		'None Selected' =>
+		'None Selected' => 
 		'Keiner',
 
-		'Please delete the old Mediathek folder from your <code>extensions</code> folder.' =>
-		'Bitte löschen Sie den alten Mediathek-Ordner in Ihrem <code>extensions</code>-Ordner.',
-
-		'Please fill out the form below.' =>
-		'Bitte ergänzen Sie nachfolgende Angaben.',
-
-		'Please make sure that the Stage submodule is initialised and available at %s.' =>
-		'Bitte überprüfen Sie, ob das Submodul Stage initialisiert wurde und unter %s zur Verfügung steht.',
-
-		'Please make sure that you have an up-to-date backup of your site, containing all files and folders, and a copy of your database.' =>
-		'Stellen Sie sicher, dass Sie eine aktuelle Sicherungskopie Ihrer Seite, die alle Dateien und Ordner beinhaltet, sowie eine Kopie Ihrer Datenbank haben.',
-
-		'Replace all Mediathek fields with the Subsection Manager, copying all attached information to the new fields.' =>
-		'Alle Mediathek-Felder werden durch Subsection-Manager-Felder ersetzt und alle verknüpften Daten in das neue Feld kopiert.',
-
-		'Stage not found' =>
-		'Stage konnte nicht gefunden werden',
-
-		'Subsection' =>
+		'Subsection' => 
 		'Unterbereich',
 
-		'Subsection Manager' =>
+		'Subsection Manager' => 
 		'Subsection Manager',
 
-		'Subsection Manager Upgrade' =>
-		'Subsection-Manager-Aktualisierung',
-
-		'Subsection Manager is a replacement of the Mediathek field, introducing a new interface and an improved feature set. Subsection Manager requires Symphony 2.1 and should not be used side-by-side with Mediathek. This page will help you upgrading your fields from Mediathek to Subsection Manager.' =>
-		'Der Subsection Manager führt ein neue, erweiterte Benutzeroberfläche ein und ersetzt das Mediathek-Feld. Der Subsection Manager setzt Symphony 2.1 voraus und sollte nicht parallel zur Mediathek eingesetzt werden. Diese Seite wird Ihnen bei der Aktualisierung von der Mediathek zum Subsection Manager helfen.',
-
-		'The Mediathek folder will stay intact in your extension folder. You will have to delete it manually.' =>
-		'Der Mediathek-Ordner wird in Ihrem Erweiterungsordner bestehen bleiben. Sie müssen ihn von Hand löschen.',
-
-		'The Subsection Manager Upgrade will automatically perform the following actions:' =>
-		'Die Subsection-Manager-Aktualisierung wird automatisch folgende Schritte ausführen:',
-
-		'There are currently no items available. Perhaps you want create one first?' =>
+		'There are currently no items available. Perhaps you want create one first?' => 
 		'Zur Zeit sind keine Einträge verfügbar. Möchten Sie zunächst einen erstellen?',
 
-		'There are no selected items' =>
+		'There are no selected items' => 
 		'Keine ausgewählten Einträge vorhanden',
 
-		'This process cannot be undone!' =>
-		'Dieser Vorgang kann nicht rückgängig gemacht werden!',
-
-		'This will add a <code>Create New</code> button to the interface' =>
-		'Dies fügt einen <code>Neu-erstellen</code>-Knopf hinzu',
-
-		'This will add a <code>Remove</code> button to the interface' =>
-		'Dies fügt einen <code>Löschen</code>-Knopf hinzu',
-
-		'This will add a search field to the interface' =>
-		'Dies fügt ein Suchfeld hinzu',
-
-		'This will enable item dragging and reordering' =>
-		'Dies ermöglicht verschieben und neuordnen von Einträgen',
-
-		'This will enable item dropping on textareas' =>
-		'Dies ermöglicht das Ziehen von Einträgen in Textfelder',
-
-		'This will switch between single and multiple item lists' =>
-		'Dies wechselt zwischen Einzel- und Mehrfacheinträgen',
-
-		'Uninstall Mediathek' =>
-		'Mediathek deinstallieren',
-
-		'Uninstall Subsection Manager' =>
-		'Subsection Manager deinstallieren',
-
-		'Uninstall the Mediathek extension removing all references in the database.' =>
-		'Die Mediathek-Erweiterung wird deinstalliert und alle Verweise in der Datenbank damit entfernt.',
-
-		'Upgrade' =>
-		'Mediathek aktualisieren',
-
-		'Upgrade all Mediathek fields' =>
-		'Alle Mediathek-Felder aktualisieren',
-
-		'Upgrade successful' =>
-		'Aktualisierung erfolgreich',
-
-		'Upgrading your Mediathek' =>
-		'Die Mediathek aktualisieren',
-
-		'Upgrading your Mediathek fields to Subsection Manager will alter your database.' =>
-		'Die Aktualisierung der Mediathek-Felder auf den Subsection Manager wird Ihre Datenbank verändern.',
-
-		'Use comma separated entry ids for filtering.' =>
-		'Verwenden Sie kommagetrennte Eintrags-IDs, um die Ausgabe zu filtern.',
-
-		'You are using Mediathek and Subsection Manager simultaneously.' =>
-		'Sie verwenden die Mediathek und den Subsection Manager gleichzeitig.',
-
-		'Your Mediathek fields have successfully been upgrade to Subsection Manager.' =>
-		'Ihre Mediathek-Felder wurden erfolgreich in Subsection-Manager-Felder umgewandelt.',
-
-		'item' =>
+		'item' => 
 		'Eintrag',
 
-		'items' =>
+		'items' => 
 		'Einträge',
 
-		'no matches' =>
+		'no matches' => 
 		'Keine Treffer',
 
-		'{$count} matches' =>
+		'{$count} matches' => 
 		'{$count} Treffer',
 
-		'It\'s available at %s.' =>
-		'Es wird unter %s zur Verfügung gestellt. ',
+		'Search existing items' => 
+		'Einträge suchen',
+
+		'Would you like to clear this tab?' => 
+		'Möchten Sie diesen Tab leeren?',
+
+		'Exclude from XML output' => 
+		'Nicht im XML ausgeben',
+
+		'Include 1 child level in XML output' => 
+		'Eine Ebene tief im XML ausgeben',
+
+		'Include 2 child levels in XML output' => 
+		'Zwei Ebenen tief im XML ausgeben',
+
+		'Include 3 child levels in XML output' => 
+		'Drei Ebenen tief im XML ausgeben',
+
+		'Nested subsections %s' => 
+		'Verschachtelte Bereiche',
+
+		'Allow removal of existing items' => 
+		'Entfernen bestehender Einträge erlauben',
+
+		'Allow multiple items' => 
+		'Mehrere Einträge erlauben',
+
+		'Enable editing' => 
+		'Bearbeiten ermöglichen',
+
+		'Enable sorting' => 
+		'Sortieren ermöglichen',
+
+		'Enable dropping' => 
+		'Ziehen ermöglichen',
+
+		'Show search' => 
+		'Suche anzeigen',
+
+		'Show image and file previews' => 
+		'Vorschaubilder anzeigen',
+
+		'Please fill out the form below' => 
+		'Bitte ergänzen Sie nachfolgende Angaben',
+
+		'\'%s\' allows single item only.' => 
+		'\'%s\' erlaubt nur einzelne Einträge',
+
+		'Use comma separated list of entry ids that has to be associated with filtered entries, e.g., "23, 45, 691" or "not: 23, 45, 691".' => 
+		'Verwenden Sie kommagetrennte Eintrags-IDs, um die Ausgabe zu filtern., z. B. "23, 45, 691" or "not: 23, 45, 691".',
+
+		'Subsection Tabs' => 
+		'Subsection Tabs',
+
+		'Tab names' => 
+		'Tabnamen',
+
+		'List of comma-separated tabs' => 
+		'Liste kommaseparierter Tabs',
+
+		'Use <code>title:</code> to filter by title or handle of an attached subsection entry.' => 
+		'Verwenden Sie <code>title:</code>, um nach Titel oder Bezeichner des angehängten Untereintrags zu filtern.',
 
 	);
